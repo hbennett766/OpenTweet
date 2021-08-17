3883037410691c1d31a5df964897300668e85ba7
Hannah Bennett <hbennett766@gmail.com>
2021-08-16 20:50:16 -0500

    build out initial cell ui + configuration

--- OpenTweet/TweetCollectionViewCell.swift
+++ OpenTweet/TweetCollectionViewCell.swift
@@ -3,6 +3,10 @@ import UIKit
 class TweetCollectionViewCell: UICollectionViewCell {
   static let reuseID = "TweetCollectionViewCellReuseID"
   
+  private var tweetLabel: UILabel!
+  private var authorLabel: UILabel!
+  private var dateLabel: UILabel!
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     sharedInit()
@@ -14,12 +18,51 @@ class TweetCollectionViewCell: UICollectionViewCell {
   }
   
   func configure(with tweet: Tweet) {
+    tweetLabel.text = tweet.content
+    authorLabel.text = tweet.author
     
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    
+    dateLabel.text = formatter.string(from: tweet.date)
   }
 }
 
 private extension TweetCollectionViewCell {
   func sharedInit() {
+    translatesAutoresizingMaskIntoConstraints = false
     
+    let stackView = UIStackView()
+    stackView.axis = .vertical
+    stackView.spacing = 16
+    
+    let padding: CGFloat = 16
+    contentView.addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding).isActive = true
+    stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding).isActive = true
+    stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding).isActive = true
+    stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding).isActive = true
+    
+    tweetLabel = UILabel()
+    tweetLabel.font = .systemFont(ofSize: 16)
+    tweetLabel.textColor = .systemGray
+    stackView.addArrangedSubview(tweetLabel)
+    
+    let authorAndDateStackView = UIStackView()
+    authorAndDateStackView.spacing = 8
+    stackView.addArrangedSubview(authorAndDateStackView)
+    
+    authorLabel = UILabel()
+    authorLabel.font = .systemFont(ofSize: 12, weight: .bold)
+    authorLabel.textColor = .systemGray5
+    authorAndDateStackView.addArrangedSubview(authorLabel)
+    
+    dateLabel = UILabel()
+    dateLabel.font = .systemFont(ofSize: 12)
+    dateLabel.textColor = .systemGray5
+    dateLabel.textAlignment = .right
+    authorAndDateStackView.addArrangedSubview(dateLabel)
   }
 }
