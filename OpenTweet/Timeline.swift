b5428a5d9734cafa656e46e8d049f022d846d26c
Hannah Bennett <hbennett766@gmail.com>
2021-08-17 07:28:43 -0500

    add thread

--- OpenTweet/Timeline.swift
+++ OpenTweet/Timeline.swift
@@ -2,6 +2,33 @@ import Foundation
 
 struct Timeline: Decodable {
   let timeline: [Tweet]
+  
+  var threads: [Thread] {
+    var threads: [Thread] = []
+    
+    var replies: [String: [Tweet]] = [:]
+    for tweet in timeline {
+      if let id = tweet.inReplyTo {
+        var existingReplies = replies[id] ?? []
+        existingReplies.append(tweet)
+        replies[id] = existingReplies
+      }
+    }
+    
+    for reply in replies {
+      let root = timeline.first { $0.id == reply.key }
+      if let root = root {
+        threads.append(Thread(root: root, replies: reply.value))
+      }
+    }
+    
+    return threads
+  }
+}
+
+struct Thread {
+  let root: Tweet
+  let replies: [Tweet]
 }
 
 struct Tweet: Decodable {
